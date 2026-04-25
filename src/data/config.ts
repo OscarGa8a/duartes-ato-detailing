8dffe398388b71577674849d6c4d189dba968ecb
Oscar Duvan Garcia Ochoa <oscar.garcia.ochoa@unillanos.edu.co>
2026-04-25 03:00:49 -0500

    feat(wine): add tertiary oxblood accent color to palette

--- src/data/config.ts
+++ src/data/config.ts
@@ -2,4 +2,7 @@ export const config = {
   phoneUSNational: "6692868649",
   phoneUSE164: "+16692868649",
   phoneFormatted: "(669) 28-68649",
+  yearsOfExperience: 6,
+  vehiclesDetailed: 10000,
+  bayAreaLocations: 1000,
 };
